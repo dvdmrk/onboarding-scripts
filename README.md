cc872eddde611d8bfec73092652676990b2fa824
davidmerk25@gmail.com <dvdmrk>
2021-11-10 13:56:25 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# onboarding-scripts
+# Mac Onboarding
+This is a quick and automated onboarding script for Mac development machines that need to work with docker and .NET.
+
+## Getting Started
+
+1. Open terminal and navigate to the folder where you keep all of your source code
+2. Clone this repo: `git clone https://github.com/dvdmrk/onboarding-scripts`
+3. Navigate into the project you just cloned: `cd onboarding-scripts/bash`
+4. Run the bash script: `sh quick-start.sh`
+
+## What's in the box?
+
+- Brew Package Manager for Mac
+- .NET SDK
+- Powershell (Required for VS Code Extensions)
+- VS Code
+- Docker
+- C# VS Code Extension
+- Docker VS Code Extension
--- bash/quick-start.sh
+++ bash/quick-start.sh
@@ -1,30 +1,29 @@
 #! /bin/bash
 clear
-echo 'Bash Onboarding Initiating'
-
+echo 'ONBOARDING AUTOMATION INITIATING'
 echo 'Installing Brew package manager for Mac...'
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 clear
 echo 'Installing .NENT SDK...'
 brew install --cask dotnet-sdk
+
 clear
 echo 'Installing Powershell...'
 brew install --cask powershell
+
 clear
 echo 'Installing VS Code...'
 brew install --cask visual-studio-code
+
 clear
 echo 'Installing Docker...'
 brew install --cask docker
-clear
-echo 'Switching to Powershell'
-pwsh
-echo 'Installing VS Extensions...'
-code --install-extension ms-dotnettools.csharp
-code --install-extension ms-azuretools.vscode-docker
 
-bash
+clear
+echo 'Installing VS Extensions...'
+pwsh ./vs-code-extensions.ps
+
 clear
 echo 'Bash Onboarding Complete'
 echo 'Navigate to your desired source directory, clone a repo, and start debugging .NET apps running in docker.'
